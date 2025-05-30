50bbf0c17e6d51888a2f9c8443b8437962413cf4
sungjoon111 <sungjoon25@yonsei.ac.kr>
2025-05-30 13:46:11 +0900

    Fix: 레이아웃 오류 해결

--- src/pages/profile/component/ProfileContactInfo.tsx
+++ src/pages/profile/component/ProfileContactInfo.tsx
@@ -2,10 +2,10 @@ import { ContactItem } from "@/types/profileTypes";
 
 const ContactInfo = ({ contacts }: { contacts: ContactItem[] }) => {
   return (
-    <section className="flex flex-col w-5/6 lg:w-3/4 h-fit items-start p-4 gap-2">
+    <section className="flex flex-col w-5/6 xl:w-3/4 h-fit items-start p-4 gap-2">
       <h1 className="text-4xl font-pre-bold">Contact</h1>
 
-      <div className="flex flex-col text-lg w-full lg:w-3/5 h-fit gap-4 mt-6">
+      <div className="flex flex-col text-lg w-full xl:w-3/5 h-fit gap-4 mt-6">
         {contacts &&
           contacts.map((contact, index) => (
             <div key={index} className="flex items-center justify-between gap-2">
