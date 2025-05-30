1d42e324672bef6cad0e04941fe170080c4cb142
sungjoon111 <sungjoon25@yonsei.ac.kr>
2025-05-31 01:45:03 +0900

    Style: 회고 간격 조정

--- src/pages/projectInfo/component/ProjectRetrospective.tsx
+++ src/pages/projectInfo/component/ProjectRetrospective.tsx
@@ -2,7 +2,7 @@ import { Portfolio } from "@/types/stackTypes";
 
 const ProjectRetrospective = ({ portfolio }: { portfolio: Portfolio }) => {
   return (
-    <section className="flex flex-col items-start justify-start w-full h-fit gap-4">
+    <section className="flex flex-col items-start justify-start w-full h-fit gap-10">
       <header className="flex items-center justify-between w-full h-fit gap-4">
         <h1 className="text-3xl font-pre-bold">회고</h1>
       </header>
